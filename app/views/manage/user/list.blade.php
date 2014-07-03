1fbcef99d1f1dd911b3f0ca915162839c8cc77cc
rose1988c <rose1988.c@gmail.com>
2014-07-03 22:29:24 +0800

    xx

--- app/views/manage/user/list.blade.php
+++ app/views/manage/user/list.blade.php
@@ -17,6 +17,49 @@
    </table>
 </div><!-- table-responsive -->
           
+<!-- Modal -->
+<button class="btn btn-primary" data-toggle="modal" data-target=".bs-example-modal-static">Launch Modal</button>
+
+<!-- Modal -->
+<div class="modal fade bs-example-modal-static" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" data-backdrop="static" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+        <div class="modal-header">
+            <button aria-hidden="true" data-dismiss="modal" class="close" type="button">&times;</button>
+            <h4 class="modal-title">Static Background</h4>
+        </div>
+        <div class="modal-body">
+            Specify static for a backdrop which doesn't close the modal on click.
+        </div>
+    </div>
+  </div>
+</div>
+<!--  -->
+<!-- Button trigger modal -->
+<button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+  Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
 @stop
 
 @section('css')
